78caf6da143290a6bdd8dac39a79cb20e6db046d
Gyozadaigaku <gaspardcube15@gmail.com>
2022-07-14 07:00:03 +0900

    add google auth func

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,14 @@
+import React, { useState } from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 import Logo from '../utils/swipy-logo.png'
+import { AiOutlineLogout } from 'react-icons/ai'
+import { BiSearch } from 'react-icons/bi'
+import { IoMdAdd } from 'react-icons/io'
+import { GoogleLogin, googleLogout } from '@react-oauth/google'
 
 const Navbar = () => {
+  const user = false
   return (
     <div className="w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4">
       <Link href="/">
@@ -15,6 +21,16 @@ const Navbar = () => {
           />
         </div>
       </Link>
+      <div>
+        {user ? (
+          <div>Logged In</div>
+        ) : (
+          <GoogleLogin
+            onSuccess={(response) => console.log(response)}
+            onError={() => console.log('Login Failed')}
+          />
+        )}
+      </div>
     </div>
   )
 }
