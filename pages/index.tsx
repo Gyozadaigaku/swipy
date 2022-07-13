d13f2c2309c4f2d42d1c9222810aab2558c77dcb
Gyozadaigaku <gaspardcube15@gmail.com>
2022-07-14 04:25:21 +0900

    add MediaCard component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import axios from 'axios'
-
+import MediaCard from '../components/MediaCard'
+import NoResults from '../components/NoResults'
 import { Media } from '../types'
 
 interface IProps {
@@ -10,7 +11,17 @@ interface IProps {
 const Home = ({ medias }: IProps) => {
   console.log(medias)
 
-  return <div className="flex flex-col gap-10 videos h-full">hello!</div>
+  return (
+    <div className="flex flex-col gap-10 videos h-full">
+      {medias.length ? (
+        medias?.map((media: Media) => (
+          <MediaCard post={media} isShowingOnHome key={media._id} />
+        ))
+      ) : (
+        <NoResults text={`No Medias`} />
+      )}
+    </div>
+  )
 }
 
 export const getServerSideProps = async () => {
