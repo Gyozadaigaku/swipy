acac74a41bc5d2b3a6478a87103c5455c98494a3
Gyozadaigaku <gaspardcube15@gmail.com>
2022-07-18 21:26:59 +0900

    add middleware for users

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,8 +8,11 @@ import { IoMdAdd } from 'react-icons/io'
 import { GoogleLogin, googleLogout } from '@react-oauth/google'
 import { createOrGetUser } from '../utils'
 
+import useAuthStore from '../store/authStore'
+
 const Navbar = () => {
-  const user = false
+  const { userProfile, addUser } = useAuthStore()
+
   return (
     <div className="w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4">
       <Link href="/">
@@ -23,12 +26,12 @@ const Navbar = () => {
         </div>
       </Link>
       <div>
-        {user ? (
-          <div>Logged In</div>
+        {userProfile ? (
+          <div>{userProfile?.userName}</div>
         ) : (
           <GoogleLogin
-            onSuccess={(response) => createOrGetUser(response)}
-            onError={() => console.log('Login Failed')}
+            onSuccess={(response) => createOrGetUser(response, addUser)}
+            onError={() => console.log('Error')}
           />
         )}
       </div>
--- utils/index.ts
+++ utils/index.ts
@@ -1,8 +1,6 @@
 import axios from 'axios'
 import jwt_decode from 'jwt-decode'
 
-export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL
-
 export const createOrGetUser = async (response: any, addUser: any) => {
   const decoded: { name: string; picture: string; sub: string } = jwt_decode(
     response.credential
@@ -15,6 +13,8 @@ export const createOrGetUser = async (response: any, addUser: any) => {
     userName: name,
     image: picture,
   }
+
+  addUser(user)
 
   await axios.post(`http://localhost:3000/api/auth`, user)
 }
