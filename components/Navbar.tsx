ba5a2461920acc34ee7fc83a0726acee1f031ef4
Gyozadaigaku <gaspardcube15@gmail.com>
2022-07-18 21:54:41 +0900

    show profile image when logged in

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -27,7 +27,27 @@ const Navbar = () => {
       </Link>
       <div>
         {userProfile ? (
-          <div>{userProfile?.userName}</div>
+          <div className="flex gap-5 md:gap-10">
+            <Link href="/upload">
+              <button className="border-2 px-2 md:px-4 text-md font-semibold flex items-center gap-2">
+                <IoMdAdd className="text-xl" />{' '}
+                <span className="hidden md:block">Upload </span>
+              </button>
+            </Link>
+            {userProfile.image && (
+              <Link href="/">
+                <div>
+                  <Image
+                    className="rounded-full cursor-pointer"
+                    src={userProfile.image}
+                    alt="user"
+                    width={40}
+                    height={40}
+                  />
+                </div>
+              </Link>
+            )}
+          </div>
         ) : (
           <GoogleLogin
             onSuccess={(response) => createOrGetUser(response, addUser)}
