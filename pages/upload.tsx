20dd50d0c40e76248a2fb44961532621f4feb416
Gyozadaigaku <gaspardcube15@gmail.com>
2022-07-19 01:33:42 +0900

    add form for uploading

--- pages/upload.tsx
+++ pages/upload.tsx
@@ -4,6 +4,7 @@ import { FaCloudUploadAlt } from 'react-icons/fa'
 import axios from 'axios'
 
 import { client } from '../utils/client'
+import { topics } from '../utils/constants'
 
 const Upload = () => {
   const [loading, setLoading] = useState<Boolean>(false)
@@ -107,6 +108,47 @@ const Upload = () => {
                 Please select a correct file
               </p>
             )}
+          </div>
+        </div>
+        <div className="flex flex-col gap-3 pb-10">
+          <label className="text-md font-medium ">Caption</label>
+          <input
+            type="text"
+            value=""
+            onChange={() => {}}
+            className="rounded lg:after:w-650 outline-none text-md border-2 border-gray-200 p-2"
+          />
+          <label className="text-md font-medium ">Choose a topic</label>
+
+          <select
+            onChange={() => {}}
+            className="outline-none lg:w-650 border-2 border-gray-200 text-md capitalize lg:p-4 p-2 rounded cursor-pointer"
+          >
+            {topics.map((item) => (
+              <option
+                key={item.name}
+                className=" outline-none capitalize bg-white text-gray-700 text-md p-2 hover:bg-slate-300"
+                value={item.name}
+              >
+                {item.name}
+              </option>
+            ))}
+          </select>
+          <div className="flex gap-6 mt-10">
+            <button
+              onClick={() => {}}
+              type="button"
+              className="border-gray-300 border-2 text-md font-medium p-2 rounded w-28 lg:w-44 outline-none"
+            >
+              Discard
+            </button>
+            <button
+              onClick={() => {}}
+              type="button"
+              className="bg-[#F51997] text-white text-md font-medium p-2 rounded w-28 lg:w-44 outline-none"
+            >
+              Post
+            </button>
           </div>
         </div>
       </div>
