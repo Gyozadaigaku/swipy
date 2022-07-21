fed922c26d4314386c9a5326c7ba6bc77f86ab12
Gyozadaigaku <gaspardcube15@gmail.com>
2022-07-21 09:13:04 +0900

    add get requests

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,12 +26,20 @@ const Home = ({ medias }: IProps) => {
   )
 }
 
-export const getServerSideProps = async () => {
-  const { data } = await axios.get(`${BASE_URL}/api/post`)
+export default Home
+
+export const getServerSideProps = async ({
+  query: { topic },
+}: {
+  query: { topic: string }
+}) => {
+  let response = await axios.get(`${BASE_URL}/api/post`)
+
+  if (topic) {
+    response = await axios.get(`${BASE_URL}/api/discover/${topic}`)
+  }
 
   return {
-    props: { medias: data },
+    props: { medias: response.data },
   }
 }
-
-export default Home
