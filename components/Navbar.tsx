5fc1744759a711503866e6f7e8a62fbae88e00fc
Gyozadaigaku <gaspardcube15@gmail.com>
2022-07-26 12:48:32 +0900

    change copy

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -52,7 +52,7 @@ const Navbar = () => {
             value={searchValue}
             onChange={(e) => setSearchValue(e.target.value)}
             className="bg-primary p-3 md:text-md font-medium border-2 border-gray-100 focus:outline-none focus:border-2 focus:border-gray-300 w-[300px] md:w-[350px] rounded-full  md:top-0"
-            placeholder="Search accounts and medias"
+            placeholder="Search"
           />
           <button
             onClick={handleSearch}
