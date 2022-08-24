88750affd40af277c0bbf7d88ffe7fc77971df3c
Gyozadaigaku <gaspardcube15@gmail.com>
2022-08-25 03:01:36 +0900

    feat: add delete button

--- src/components/molecules/PopupMenu/PopupMenu.tsx
+++ src/components/molecules/PopupMenu/PopupMenu.tsx
@@ -1,6 +1,8 @@
 import React, { useState, useRef } from 'react'
 import useOutsideAlerter from '@/components/hooks/useOutsideAlerter'
 
+import { MdDelete } from 'react-icons/md'
+
 const PopupMenu = () => {
   const [isShown, setIsShown] = useState(false)
   const wrapperRef = useRef(null)
@@ -17,15 +19,20 @@ const PopupMenu = () => {
         Toggle Menu
       </button>
       <ul
-        className={`absolute z-[1000] origin-top-left w-40 mt-2 p-4 bg-white rounded-lg transition duration-200 ${
+        className={`absolute z-[1000] origin-top-left w-40 mt-2 py-2 bg-white rounded-lg transition duration-200 shadow ring-1 ring-slate-900/5 ${
           isShown ? 'scale-1' : 'scale-0'
         }`}
         id="menuList"
         role="menu"
       >
-        <li role="presentation">menu</li>
         <li role="presentation">
-          <button onClick={() => setIsShown(false)}>Close Menu</button>
+          <button
+            className=" hover:bg-gray-100 inline-flex items-center py-1 px-2 w-full"
+            onClick={() => setIsShown(false)}
+          >
+            <MdDelete className="mr-1" />
+            <span>Delete</span>
+          </button>
         </li>
       </ul>
     </div>
